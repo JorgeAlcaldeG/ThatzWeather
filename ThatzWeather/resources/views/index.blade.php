269a319f76f7ab7ab83e7b8f98afc9f76481f71e
JorgeAlcaldeG <jorge-2806@hotmail.com>
2023-09-29 12:30:36 +0200

    Página home adaptado a móvil

--- ThatzWeather/resources/views/index.blade.php
+++ ThatzWeather/resources/views/index.blade.php
@@ -10,7 +10,7 @@
     <img src="{{ asset('img/home/logo.png') }}" class="centerImg" id="logo">
     <p id="homeText" class="centerText">Entérate del tiempo en la zona exacta que te interesa buscando por código postal.</p>
     <form action="/data" method="get" class="center" onsubmit="return validarHome()">
-        <input type="number" name="cp" id="homeInput" class="center homeInput homeInput" placeholder="Introduce el código postal">
+        <input type="number" name="cp" id="homeInput" class="center homeInput" placeholder="Introduce el código postal">
         <p class="centerText" id="errorText">¡El código postal indicado no es correcto!</p>
         <button type="submit" class="center" id="homeButton">Buscar <img src="{{ asset('img/home/searchIcon.png') }}" id="searchIcon"></button>
     </form>
